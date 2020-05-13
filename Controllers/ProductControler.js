f8bbd594da0cc6be595cb50c05bb8cecc7e94e52
Muhammad Taufiq Hidayat <mtfiqh@gmail.com>
2020-05-13 10:13:31 +0700

    index on product controller

---
+++ Controllers/ProductControler.js
@@ -0,0 +1,11 @@
+const model = new (require('../Models/Product'))()
+const {resHelper} = require('../Helpers/ResponseHelper')
+module.exports = class Product{
+
+    async index(req,res){
+        let data = await model.get()
+        if(Object.keys(data).length<1) return res.send(resHelper({}, "Product Empty"),404)
+        return res.send(resHelper(data, "All products fetched"),200)
+    }
+
+}
