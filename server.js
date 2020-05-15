c741553bbe37f7be7ba53ccaeb8858568e5f4098
Muhammad Taufiq Hidayat <mtfiqh@gmail.com>
2020-05-15 14:43:05 +0700

    allow-headers cors

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ app.use(express.static('./public'))
 // use
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // prefix
 const prefix = '/api/v1'
 
