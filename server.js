8ba65cceee3c6b3b887f7c7935740c2645f9f703
Muhammad Taufiq Hidayat <mtfiqh@gmail.com>
2020-05-15 21:53:02 +0700

    install cors to fix token header

--- server.js
+++ server.js
@@ -9,18 +9,20 @@ const git = require('simple-git/promise')
 // import
 const express = require('express')
 const bodyParser = require('body-parser');
-
+const cors = require('cors')
 // define object
 const app = express()
+app.use(cors())
 app.use(express.static('./public'))
 // use
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 // prefix
 const prefix = '/api/v1'
 
